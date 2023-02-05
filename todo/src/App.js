8dad841ffb7dafa95567b39889b6640c4474a471
Fabien Thiombiano <fabien@MacBook-Pro.local>
2023-02-05 00:12:06 +0000

    added state data with state changes

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "Adam",
+    };
+  }
+
+  changeStateData = () => {
+    this.setState({
+      username: this.state.username === "Adam" ? "Bob" : "Adam",
+    });
+  };
+
+  render = () => (
+    <div>
+      <h4 className="bg-primary text-white text-center p-2">{this.state.username}'s To Do List</h4>
+      <button className="btn btn-primary m-2" onClick={this.changeStateData}>
+        Change
+      </button>
     </div>
   );
 }
-
-export default App;
